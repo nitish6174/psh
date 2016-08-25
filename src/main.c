869f987089cff028afed5074d8995aa6b354d736
Sambhav Kothari <sambhavs.email@gmail.com>
2016-08-25 11:53:32 +0530

    Print cwd before each command

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 /* Function Declarations */
 int psh_cd(char **args);
@@ -219,10 +226,10 @@ void psh_loop(void)
 	char *line;
 	char **args;
 	int status;
-
+	char cwd[10240];
 	do
 	{
-		printf("> ");
+		printf(ANSI_COLOR_GREEN"%s"ANSI_COLOR_RESET"> ",getcwd(cwd,sizeof(cwd)));
 		line = psh_read_line();
 		args = psh_split_line(line);
 		status = psh_execute(args);
