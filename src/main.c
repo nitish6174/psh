04d47299c063f02a6ee4a131a15b2a74a6ca014a
Sambhav Kothari <sambhavs.email@gmail.com>
2016-08-25 14:56:34 +0530

    Splits functions into files

--- src/main.c
+++ src/main.c
@@ -12,40 +12,6 @@
 #include "history.h"
 
 
-
-
-
-
-/*Update History*/
-void update_history(char *buffer)
-{
-	FILE *fp;
-	if( access( "history", F_OK ) != -1 ) 
-	{
-		fp = fopen("history", "r+");
-		int lines;
-		fscanf(fp,"%d\n",&lines);
-		lines++;
-		fseek(fp,0,SEEK_SET);
-		fprintf(fp, "%d\n",lines );
-		fclose(fp);
-	}
-
-	else 
-	{
-		fp = fopen("history", "w");
-		fprintf(fp, "%d\n",1 );
-		fclose(fp);
-	}
-
-	fp = fopen("history", "a+");
-	fprintf(fp, "%s\n",buffer );
-	fclose(fp);
-	return;
-}
-
-
-
 /* Loop to get input and execute it */
 void psh_loop(void)
 {
