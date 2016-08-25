a0a2d0aa35bb6f48953bdf47e26d88fb722f416b
Nitish Garg <nitish.garg.6174@gmail.com>
2016-08-25 13:12:58 +0530

    Added formatted printing function

---
+++ src/print_format.h
@@ -0,0 +1,60 @@
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+char *formatCode(char *style, char *color)
+{
+
+	char *code;
+	char pre[]="\033[";
+	char style_code[3], color_code[3];
+
+
+	if(strcmp(style,"normal")==0)
+		strcpy(style_code,"0");
+	else if(strcmp(style,"bold")==0)
+		strcpy(style_code,"1");
+	else if(strcmp(style,"dim")==0)
+		strcpy(style_code,"2");
+	else if(strcmp(style,"italic")==0)
+		strcpy(style_code,"3");
+	else if(strcmp(style,"underline")==0)
+		strcpy(style_code,"4");
+	else
+		strcpy(style_code,"0");
+
+
+	if(strcmp(color,"black")==0)
+		strcpy(color_code,"30");
+	else if(strcmp(color,"red")==0)
+		strcpy(color_code,"31");
+	else if(strcmp(color,"green")==0)
+		strcpy(color_code,"32");
+	else if(strcmp(color,"yellow")==0)
+		strcpy(color_code,"33");
+	else if(strcmp(color,"blue")==0)
+		strcpy(color_code,"34");
+	else if(strcmp(color,"purple")==0)
+		strcpy(color_code,"35");
+	else if(strcmp(color,"cyan")==0)
+		strcpy(color_code,"36");
+	else if(strcmp(color,"white")==0)
+		strcpy(color_code,"37");
+	else
+		strcpy(color_code,"0");
+
+
+	code = malloc( strlen(pre)+strlen(style_code)+strlen(color_code)+1 );
+	code[0] = '\0';
+	strcat(code,pre);
+	strcat(code,style_code);
+	strcat(code,";");
+	strcat(code,color_code);
+	strcat(code,"m");
+	return code;
+}
